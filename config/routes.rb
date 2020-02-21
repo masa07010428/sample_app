e8dd6c89839fb0b631004bb1618550138354a0df
Masaru Kubo <mkmkmk04281209@gmail.com>
2020-02-22 08:43:20 +0900

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,22 @@
 Rails.application.routes.draw do
+  
   root 'static_pages#home'
   
   # root 'static_pages#about'
   
   # root 'static_pages#help'
-
+  
   # root 'static_pages#contact'
   
   # root 'static_pages#signup'
   
   # root 'static_pages#login'
-
+  
   get '/help', to: 'static_pages#help'
   
   get '/about', to: 'static_pages#about'
-
+  
   get '/contact', to: 'static_pages#contact'
+  
+  get '/signup', to: 'users#new'
 end
