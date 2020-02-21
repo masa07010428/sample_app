7cf22f48aa71c753189845163d72fa66329a2ae2
Masaru Kubo <mkmkmk04281209@gmail.com>
2020-02-21 21:38:55 +0900

    modify path

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,19 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   
-  root 'static_pages#about'
+  # root 'static_pages#about'
   
-  root 'static_pages#help'
+  # root 'static_pages#help'
 
-  root 'static_pages#contact'
+  # root 'static_pages#contact'
   
-  root 'static_pages#signup'
+  # root 'static_pages#signup'
   
-  root 'static_pages#login'
+  # root 'static_pages#login'
 
-  get 'static_pages/home'
-
-  get 'static_pages/help'
+  get '/help', to: 'static_pages#help'
   
-  get 'static_pages/about'
+  get '/about', to: 'static_pages#about'
 
-  get 'static_pages/contact'
-
-
-  
+  get '/contact', to: 'static_pages#contact'
 end
