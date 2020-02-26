48a00a1843156c117d80988588e4a406629b421b
Masaru Kubo <mkmkmk04281209@gmail.com>
2020-02-27 06:51:51 +0900

    sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Sample App"
+      # redirect_to user_url(@user)
+      redirect_to @user
     else
       render 'new'
     end
