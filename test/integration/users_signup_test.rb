86ac52f167005706ed8434a52f2cafdc0c1298a1
Masaru Kubo <mkmkmk04281209@gmail.com>
2020-02-28 19:52:14 +0900

    before deploy

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -7,10 +7,12 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post signup_path, params: { user: { name: "",
+      post signup_path, params: { user: {
+        name: "",
         email: "user@invalid",
         password: "foo",
-        password_confirmation: "bar" } }
+        password_confirmation: "bar"
+        } }
     end
     assert_template 'users/new'
     assert_select 'div#error_explanation'
@@ -19,10 +21,12 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   end
   test "valid signup information" do
     assert_difference 'User.count',1 do
-      post users_path,params: { user: { name: "Example User",
+      post users_path,params: { user: {
+        name: "Example User",
         email: "user@example.com",
         password: "password",
-        password_confirmation: "password" } }
+        password_confirmation: "password"
+        } }
     end
     follow_redirect!
     assert_template 'users/show'
