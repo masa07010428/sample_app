12c62bfaa8b4744853ac626f14109fedf02c288f
Masaru Kubo <mkmkmk04281209@gmail.com>
2020-02-29 07:33:48 +0900

    basic-login

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,8 @@
 Rails.application.routes.draw do
   
+
   root 'static_pages#home'
-  
-  # root 'static_pages#about'
-  
-  # root 'static_pages#help'
-  
-  # root 'static_pages#contact'
-  
-  # root 'static_pages#signup'
-  
-  # root 'static_pages#login'
-  
+    
   get '/help', to: 'static_pages#help'
   
   get '/about', to: 'static_pages#about'
@@ -21,6 +12,12 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
 
   post '/signup', to: 'users#create'
+
+  get '/login', to: 'sessions#new'
+
+  post '/login', to: 'sessions#create'
+
+  delete 'logout', to: 'sessions#destroy'
 
   resources :users
 end
