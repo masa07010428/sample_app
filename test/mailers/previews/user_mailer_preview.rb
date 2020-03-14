80215457d99971a24bbefb16bf0339dcc20a1674
Masaru Kubo <mkmkmk04281209@gmail.com>
2020-03-14 19:22:08 +0900

    passward-reset

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,16 +1,18 @@
-# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer.html
 class UserMailerPreview < ActionMailer::Preview
 
-  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/account_activation
+  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/account_activation.html
   def account_activation
     user = User.first
     user.activation_token = User.new_token
     UserMailer.account_activation(user)
   end
 
-  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
+  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset.html
   def password_reset
-    UserMailer.password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
 
 end
